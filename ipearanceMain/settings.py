521e7a714277e59532b1c70a34dcaf5ee7befb27
DESKTOP-AQ46HRP\Par <vaslysalex@hotmail.gr>
2021-09-11 15:39:00 +0300

    fifth9

--- ipearanceMain/settings.py
+++ ipearanceMain/settings.py
@@ -15,6 +15,7 @@ import os
 
 import django_heroku
 import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
